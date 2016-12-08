8c61083142ddac59e3be7b1ea7c63e36cf4157d5
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2016-12-08 20:36:40 +0530

    PHP 7.1.0 release

--- linux-build-install.sh
+++ linux-build-install.sh
@@ -10,7 +10,7 @@ set -e
 mkdir -p work
 cd work
 
-curl -fsSL http://www.php.net/distributions/php-7.0.13.tar.xz | tar -xJf - --strip-components=1
+curl -fsSL http://www.php.net/distributions/php-7.1.0.tar.xz | tar -xJf - --strip-components=1
 
 ./configure \
     --disable-cgi \
