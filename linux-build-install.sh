e10e7dc28ea8591631d8fba699ce325cb47e9f94
Dylan K. Taylor <odigiman@gmail.com>
2017-02-06 19:42:34 +0000

    Bump WeakRef version to 0.3.3

--- linux-build-install.sh
+++ linux-build-install.sh
@@ -28,7 +28,7 @@ curl -fsSL http://www.php.net/distributions/php-7.0.15.tar.xz | tar -xJf - --str
 make -j`nproc`
 $SUDO make install
 
-$SUDO "${PREFIX}/bin/pecl" install channel://pecl.php.net/pthreads-3.1.6 channel://pecl.php.net/weakref-0.3.2 channel://pecl.php.net/yaml-2.0.0
+$SUDO "${PREFIX}/bin/pecl" install channel://pecl.php.net/pthreads-3.1.6 channel://pecl.php.net/weakref-0.3.3 channel://pecl.php.net/yaml-2.0.0
 echo "phar.readonly = off
 extension = yaml.so
 extension = pthreads.so
