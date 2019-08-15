9c4868c52891dd793e9515ac9a54f34a95f7cd23
PeratX <1215714524@qq.com>
2019-08-15 16:09:25 +0800

    Update PHP 7.3.8 and dependencies

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ## Script for building Windows PHP packages
 
 The script need to be ran on Linux.
-Dependencies: aria2, bsdtar
 
 ## Script for building and installing PHP on Linux
 
@@ -9,10 +8,10 @@ Run this to build and install PHP configure to match your environment.
 You need basic compiler package group, build dependencies and curl, sudo, tar.
 
 The following is just for reference:  
-* For Debian-based distros: `libssl-dev autoconf pkg-config curl libedit-dev libsqlite3-dev libxml2-dev libcurl4-openssl-dev libyaml-dev libzip-dev libgmp-dev`
+* For Debian-based distros: `libssl-dev autoconf pkg-config curl libedit-dev libsqlite3-dev libxml2-dev libcurl4-openssl-dev libyaml-dev libzip-dev libgmp-dev libsodium-dev libjpeg-dev libpng-dev libwebp-dev libfreetype6-dev`
 * For RPM-based-distros: `autoconf pkg-config curl libedit-devel libsqlite3-devel libxml2-devel libyaml-devel libcurl-devel libzip-last-devel libgmp-devel`
 
-* If you are using Ubuntu 17.04 and above, configure can not locate the libcurl, so you need to `sudo ln -s /usr/include/x86_64-linux-gnu/curl /usr/include`
+* If you are using Ubuntu 17.04, configure can not locate the libcurl, so you need to `sudo ln -s /usr/include/x86_64-linux-gnu/curl /usr/include`
 
 Environment variable "$PREFIX" can be set to install to other locations (must be absolute path). The default is /usr/local, which most time has the highest priority in the PATH.
 
---
+++ install.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Requirements: curl, sudo, tar, building tools
+
+PREFIX=/usr/local
+SUDO=sudo
+DL=wget
+PROXYCHAINS=
+
+PHP_VERSION=7.3.8
+YAML_VERSION=2.0.4
+SWOOLE_VERSION=4.4.3
+ZIP_VERSION=1.15.4
+
+set -e
+
+mkdir -p temp
+cd temp
+
+$PROXYCHAINS $DL http://www.php.net/distributions/php-$PHP_VERSION.tar.xz
+tar -xJf php-$PHP_VERSION.tar.xz
+cd php-$PHP_VERSION
+
+echo \
+    --disable-cgi \
+    --enable-mbstring \
+    --enable-bcmath \
+    --enable-pdo \
+    --with-mysql-pdo \
+    --with-sodium \
+    --enable-sockets \
+    --with-curl \
+    --with-libedit \
+    --with-openssl \
+    --with-zlib \
+    --with-gmp \
+    --with-gd \
+    --with-jpeg-dir \
+    --with-webp-dir \
+    --with-png-dir \
+    --with-freetype-dir \
+    --enable-pcntl \
+    --enable-maintainer-zts \
+    --prefix="$PREFIX"
+
+make -j`nproc`
+$SUDO make install
+cd ..
+
+$PROXYCHAINS git clone https://github.com/krakjoe/pthreads.git --depth=1
+cd pthreads
+phpize
+./configure
+make -j'nproc'
+$SUDO make install
+cd ..
+
+$PROXYCHAINS $DL https://pecl.php.net/get/swoole-$SWOOLE_VERSION.tgz
+tar -zxf swoole-$SWOOLE_VERSION.tgz
+cd swoole-$SWOOLE_VERSION
+phpize
+./configure --enable-http2 --enable-sockets --enable-openssl
+make -j'nproc'
+$SUDO make install
+cd ..
+
+$PROXYCHAINS git clone https://github.com/swoole/ext-async.git --depth=1
+cd ext-async
+phpize
+./configure
+make -j'nproc'
+$SUDO make install
+cd ..
+
+$PROXYCHAINS $DL https://pecl.php.net/get/yaml-$YAML_VERSION.tgz
+tar -zxf yaml-$YAML_VERSION.tgz
+cd yaml-$YAML_VERSION
+phpize
+./configure
+make -j'nproc'
+$SUDO make install
+cd ..
+
+$PROXYCHAINS $DL https://pecl.php.net/get/zip-$ZIP_VERSION.tgz
+tar -zxf zip-$ZIP_VERSION.tgz
+cd zip-$ZIP_VERSION
+phpize
+./configure
+make -j'nproc'
+$SUDO make install
+cd ..
+
+$PROXYCHAINS git clone https://github.com/runkit7/runkit7.git --depth=1
+cd runkit7
+phpize
+./configure
+make -j'nproc'
+$SUDO make install
+cd ..
+
+echo "phar.readonly = off
+extension = yaml.so
+extension = pthreads.so
+extension = swoole.so
+extension = swoole_async.so
+extension = runkit.so
+extension = zip.so
+zend_extension = opcache.so
+zend.assertions = -1
+" | $SUDO tee "$PREFIX/lib/php.ini" > /dev/null
+
+cd ..
+rm -rf temp
